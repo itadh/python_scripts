ad652c5cd6e281d3e1a9c8c59430a63fbd414b56
Jörg Zimmermann <jz@mgeg.de>
2021-01-11 08:03:20 +0100

    add output

---
+++ output.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+print('Hallo Welt')
+
+print(4+7)
+
+print('-' * 40)
+
+farbe = 'red'
+alter = 33
+
+auto = 'Ente'
+print(auto)
+
+
+print(farbe)
+print(alter)
+
+print(auto)
+
+# declare int(4) alter 
+# declare char(12) auto
+
+
