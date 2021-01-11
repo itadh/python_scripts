3947e383900fcd5d9a5a5d00a45dca665e8ab607
Jörg Zimmermann <jz@mgeg.de>
2021-01-11 08:03:48 +0100

    count things

---
+++ count_things.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+satz = input('Bitte geben Sie einen Satz ein: ')
+words = satz.split(' ')
+print(len(words))
+
---
+++ count_things2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+satz = input('Bitte geben Sie einen Satz ein: ')
+print(len(satz))
+
