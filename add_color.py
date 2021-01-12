b7ebee77582977c90f5b3a253806cba111e23581
Jörg Zimmermann <jz@mgeg.de>
2021-01-12 09:53:26 +0100

    add add color

---
+++ add_color.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+farben = []
+farbe = ''
+while farbe != 'q':
+    if farbe != '':
+        farben.append(farbe)
+    farbe = input('Bitte geben Sie eine Farbe ein: ')
+print(farben)
+
