d085612c627745f0b4b0bfe413572c3ed3e2f9c8
Jörg Zimmermann <jz@mgeg.de>
2021-01-12 11:07:47 +0100

    improve script

--- add_color.py
+++ add_color.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 farben = []
 farbe = ''
-while farbe != 'q':
-    if farbe != '':
-        farben.append(farbe)
+while True:
+    if farbe == 'q':
+        break
     farbe = input('Bitte geben Sie eine Farbe ein: ')
+#    if farbe != 'q' and farbe != '':
+#        farben.append(farbe)
+    if farbe == 'q' or farbe == '':
+        continue
+    farben.append(farbe)
+
 print(farben)
 
