d2b3236dffdc043dec39a2af836f5cfc2a836fc8
Jörg Zimmermann <jz@mgeg.de>
2021-01-12 09:36:14 +0100

    add phrases2

--- phrase_upper2.py
+++ phrase_upper2.py
@@ -2,7 +2,6 @@
 satz = input('Bitte geben Sie einen Satz ein: ')
 woerter = satz.split()
 for word in woerter:
-    print(word.upper())
     line = ''
     for zeichen in word.upper():
         line = f'{line} {zeichen}'
