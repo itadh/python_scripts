f83f50709935068f621e4867cd8a9d818fd023e2
Jörg Zimmermann <jz@mgeg.de>
2021-01-18 15:09:50 +0100

    add: fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,41 @@
 #!/usr/bin/python3
+print('0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946, ...')
+
+eingabe = input('Wieviele Fibonacci-Zahlen sollen berechnet werden: ')
+eingabe = int(eingabe)
+
+startwert1 = 0
+startwert2 = 1
+
+zaehler = 0
+while zaehler < eingabe:
+    print(f'Durchlauf {zaehler}: {startwert1}')
+    if zaehler < eingabe:
+        ergebnis = startwert1 + startwert2
+        startwert1 = startwert2
+        startwert2 = ergebnis
+        zaehler = zaehler + 1
+#    print(f'Durchlauf {zaehler}: {ergebnis}')
+
+# zaehler = 0
+# startwert1 = 0
+# startwert2 = 1
+# ergebnis = 1
+# 
+# zaehler = 1
+# startwert1 = 1 (startwert2)
+# startwert2 = 1 (ergebnis vorheriger Durchlauf)
+# ergebnis = 2
+# 
+# zaehler = 2
+# startwert1 = 1 (startwert2)
+# startwert2 = 2 (ergebnis vorheriger Durchlauf)
+# ergebnis = 3
+# 
+# zaehler = 3
+# startwert1 = 2 (startwert2)
+# startwert2 = 3 (ergebnis vorheriger Durchlauf)
+# ergebnis = 5
+# 
+
 
